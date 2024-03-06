7e3e5d37a9b6a822dad4d870d3d8cbdb4dd51957
Robertino Garcia <140854280+RobertinoS@users.noreply.github.com>
2024-03-06 11:16:02 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -14,23 +14,23 @@ data.loc[data['Cuenta'].str.contains('Puntuales|Socios|Muni|Ingresos|Interes|Cob
 st.title("Dashboard de la Empresa")
 
 # Mostrar los datos en una tabla
-'''st.subheader("Datos de la Empresa")
-st.write(data)'''
+st.subheader("Vivienda Digna")
+st.write(data)
 
 # Gráfico interactivo de pastel para la categoría Gastos en Personal
-st.subheader("Diagrama de Pastel: Gastos en Personal")
+st.subheader("Gastos en Personal")
 gastos_personal_counts = data[data['Categoria'] == 'Gastos en Personal']['Cuenta'].value_counts()
 fig_gastos_personal = px.pie(values=gastos_personal_counts, names=gastos_personal_counts.index, title="Gastos en Personal")
 st.plotly_chart(fig_gastos_personal, use_container_width=True)
 
 # Gráfico interactivo de pastel para la categoría Ingresos
-st.subheader("Diagrama de Pastel: Ingresos")
+st.subheader("Ingresos")
 ingresos_counts = data[data['Categoria'] == 'Ingresos']['Cuenta'].value_counts()
 fig_ingresos = px.pie(values=ingresos_counts, names=ingresos_counts.index, title="Ingresos")
 st.plotly_chart(fig_ingresos, use_container_width=True)
 
 # Gráficos interactivos de barras para Imp. presupuestado, Imp. real y Desvio
-st.subheader("Gráficos de Barras: Imp. Presupuestado, Imp. Real, Desvio")
+st.subheader("Imp. Presupuestado, Imp. Real, Desvio")
 selected_column = st.selectbox("Seleccione una columna:", options=['Imp. presupuestado', 'Imp. real', 'Desvio'])
 fig_bar = px.bar(data, x='Cuenta', y=selected_column, title=f"{selected_column} por Cuenta", width=800, height=500)
 st.plotly_chart(fig_bar, use_container_width=True)
